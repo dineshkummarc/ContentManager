3e1400bf8040ead493e3074e4f0396c57f81621d
nick <eiu165@gmail.com>
2009-12-12 10:44:36 -0600

    first test

---
+++ Source/Content.Web.Tests/Services/ContentServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Content.Web.Code.Service.Base;
+using Content.Web.Code.Entities;
+using Content.Web.Code.DataAccess.Fake;
+
+namespace Content.Web.Tests.Services
+{
+    [TestClass]
+    public class ContentServiceTest
+    { 
+
+        [TestMethod]
+        public void ContentService_Get_IsNotNull()
+        {
+            ContentService cs = new ContentService(new FakeContentRepository());
+            IQueryable<HtmlContent> contents =  cs.GetContent();
+            Assert.IsNotNull(contents);
+        }
+    }
+}
