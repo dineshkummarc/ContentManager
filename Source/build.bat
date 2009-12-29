5f2d0f71de57f8efd60fe7bbfb7e65dc3ad6b1f7
eiu165 <eiu165@gmail.com>
2009-12-29 15:46:09 -0600

    can go to command line and type    'build'

--- Source/build.bat
+++ Source/build.bat
@@ -1,4 +1,25 @@
 @echo off
+REM *****************************************
+REM       usage: go to command line and type   'build'
+REM	      for a specific target use 'build target'
+REM       example   'build Zip'
+REM
+REM		  old code REM msbuild Content.build /t:%*
+REM *****************************************
 cls
-REM msbuild Content.build %*
-C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild Content.build %*
+
+echo -----------------------------
+set msBuildCommand=C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild Content.build
+
+if "%1"=="" GOTO runDefault
+	echo about to run with target ='%1'
+	echo -----------------------------
+	%msBuildCommand%  /t:%1
+GOTO end
+
+:runDefault
+	echo inside runDefault target ='%1'
+	echo -----------------------------
+	%msBuildCommand% 
+
+:end 
