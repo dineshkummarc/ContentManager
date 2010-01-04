a3614086b75c7925a585d3f3b39e45f06fea0e24
eiu165 <eiu165@gmail.com>
2010-01-04 09:55:39 -0600

    move the build scripts

---
+++ Source/Content.Sql/DropDb.sql
@@ -0,0 +1,9 @@
+USE [master]
+GO
+
+/****** Object:  Database [Test]    Script Date: 12/30/2009 10:00:23 ******/
+IF  EXISTS (SELECT name FROM sys.databases WHERE name = N'Test')
+DROP DATABASE [Test]
+GO
+
+
