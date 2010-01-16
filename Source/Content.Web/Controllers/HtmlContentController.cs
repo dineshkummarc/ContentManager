d4c3a5eb9c2512e00565ac22e6bcc3a55b3d231c
nick <eiu165@gmail.com>
2010-01-15 21:43:46 -0600

    getting this working for golive by removing openid

--- Source/Content.Web/Controllers/HtmlContentController.cs
+++ Source/Content.Web/Controllers/HtmlContentController.cs
@@ -21,6 +21,7 @@ namespace ContentNamespace.Web.Controllers
             this._service = service;
         }
 
+        [Authorize(Roles = "Admin")]
         public ActionResult Index()
         {
             return View(this._service.Get());
@@ -28,6 +29,7 @@ namespace ContentNamespace.Web.Controllers
 
         //
         // GET: /HtmlContent/Details/5 
+        [Authorize(Roles = "Admin")]
         public ActionResult Details(int id)
         {
             return View(this._service.Get(id));
@@ -35,16 +37,16 @@ namespace ContentNamespace.Web.Controllers
 
         //
         // GET: /HtmlContent/Create
-
+        [Authorize(Roles = "Admin")]
         public ActionResult Create()
         {
             return View();
         }
 
         //
-        // POST: /HtmlContent/Create
-
+        // POST: /HtmlContent/Create 
         [AcceptVerbs(HttpVerbs.Post)]
+        [Authorize(Roles = "Admin")]
         public ActionResult Create([Bind(Exclude = "Id")] HtmlContent c)
         {  
             c.ModifiedBy = "XXXX";//TODO: should be loged in user
@@ -62,6 +64,7 @@ namespace ContentNamespace.Web.Controllers
 
         //
         // GET: /HtmlContent/Edit/5 
+        [Authorize(Roles = "Admin")]
         public ActionResult Edit(int id)
         {
             var editContent = this._service.Get(id);
@@ -75,6 +78,7 @@ namespace ContentNamespace.Web.Controllers
         // POST: /HtmlContent/Edit/5 
         [AcceptVerbs(HttpVerbs.Post)]
         [ValidateInput(false)]
+        [Authorize(Roles = "Admin")]
         public ActionResult Edit(int id, [Bind(Exclude = "Id")] HtmlContent c)
         {
             try
@@ -93,6 +97,7 @@ namespace ContentNamespace.Web.Controllers
 
         //
         // GET: /HtmlContent/EditExtra/5 
+        [Authorize(Roles = "Admin")]
         public ActionResult EditExtra(int id)
         {
             var editContent = this._service.Get(id);
@@ -105,6 +110,7 @@ namespace ContentNamespace.Web.Controllers
         //ref: http://tinyurl.com/d6xolp      
         // POST: /HtmlContent/EditExtra/5 
         [AcceptVerbs(HttpVerbs.Post)]
+        [Authorize(Roles = "Admin")]
         public ActionResult EditExtra(int id, [Bind(Exclude = "Id,ContentData")] HtmlContent c)
         {
             try
@@ -121,8 +127,7 @@ namespace ContentNamespace.Web.Controllers
                 return View();
             }
         }
-
-
+ 
         public ActionResult Content(string id)
         {
             var c = this._service.Get(Convert.ToInt32(id));
--- Source/Content.Web/Views/Home/About.aspx
+++ Source/Content.Web/Views/Home/About.aspx
@@ -10,3 +10,16 @@
         Put content here.
     </p>
 </asp:Content>
+
+
+<asp:Content ID="Content4" ContentPlaceHolderID="JavaScript" runat="server">
+    <script type="text/javascript">
+        $(function() {
+            $('.ContentArea').each(function() { 
+                $(this).load('/HtmlContent/ContentPage/' + $(this).attr('contentId'));
+                //$(this).load('<%= Url.Action("ContentPage", new {id = 0}) %>');
+            });  
+        });  
+    </script>
+</asp:Content>
+
